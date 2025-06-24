e10558930f3287b0cf90d06da6f88a1942a2ee7a
RCAPIV1 <bsm.bills@gmail.com>
2025-06-24 14:48:33 +0530

    initial commit

---
+++ src/main/java/JavaPrograms/Welcome.java
@@ -0,0 +1,6 @@
+package JavaPrograms;
+public class Welcome {
+    public static void main(String[] args){
+        System.out.println("Welcome to java");
+    }
+}
---
+++ src/test/java/JavaTesting/WelcomeTest.java
@@ -0,0 +1,7 @@
+package JavaTesting;
+
+public class WelcomeTest {
+	public static void main(String[] args) {
+		System.out.println("Welcome to java Test");
+	}
+}
