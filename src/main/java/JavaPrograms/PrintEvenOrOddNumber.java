356615b497e3353863555678a60a9194635c873e
RCAPIV1 <bsm.bills@gmail.com>
2025-06-26 12:42:05 +0530

    developed PrintEvenOrOddNumber program

---
+++ src/main/java/JavaPrograms/PrintEvenOrOddNumber.java
@@ -0,0 +1,12 @@
+package JavaPrograms;
+
+public class PrintEvenOrOddNumber {
+    public static void main(String[] args){
+        int num = -4;
+        if(num % 2 == 0){
+            System.out.println(num+" is even number");
+        }else {
+            System.out.println(num+" is odd number");
+        }
+    }
+}
