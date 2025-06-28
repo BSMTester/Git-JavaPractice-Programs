5848c252bdd68d9d14dd5d5c74e03bd2b761d775
RCAPIV1 <bsm.bills@gmail.com>
2025-06-28 17:41:54 +0530

    Modified SumOfTemnumbers as SumOfNumbers

---
+++ src/main/java/JavaPrograms/SumOfNumbers.java
@@ -0,0 +1,11 @@
+package JavaPrograms;
+
+public class SumOfNumbers {
+	public static void main(String[] args) {
+	int sum = 0;
+	for(int i =1;i<=15;i++) {
+		sum = sum + i;
+	}
+	System.out.println("sum of one to fifteen numbers: "+sum);
+	}
+}
